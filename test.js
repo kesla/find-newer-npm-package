3bf442603bcd7475c03f833dfc0edb66a2c97413
David Björklund <david.bjorklund@gmail.com>
2016-06-08 17:22:38 -0400

    more tests

--- test.js
+++ test.js
@@ -1,15 +1,23 @@
 import test from 'tapava';
 import find from './lib';
 
-test('find() empty', t => {
-  const input = [];
+test('find() empty packages', t => {
+  const input = ['1.0.0'];
   const packages = [];
   const actual = find(input, packages);
   const expected = [];
   t.deepEqual(actual, expected);
 });
 
-test('find() simple ~', t => {
+test('find() empty input', t => {
+  const input = [];
+  const packages = ['1.0.0', '2.0.0'];
+  const actual = find(input, packages);
+  const expected = ['2.0.0'];
+  t.deepEqual(actual, expected);
+});
+
+test('find() ~', t => {
   const input = ['1.0.0'];
   const packages = ['1.0.0', '1.0.2', '1.0.1', '2.0.0'];
   const actual = find(input, packages);
@@ -17,7 +25,7 @@ test('find() simple ~', t => {
   t.deepEqual(actual, expected);
 });
 
-test('find() simple ^', t => {
+test('find() ^', t => {
   const input = ['1.0.0'];
   const packages = ['1.0.0', '1.1.0', '1.2.0', '2.0.0'];
   const actual = find(input, packages);
@@ -25,10 +33,18 @@ test('find() simple ^', t => {
   t.deepEqual(actual, expected);
 });
 
-test('find() simple ^ & ~', t => {
+test('find() ^ and ~', t => {
   const input = ['1.0.0'];
   const packages = ['1.0.0', '1.0.1', '1.0.2', '1.1.0', '1.2.0', '2.0.0'];
   const actual = find(input, packages);
   const expected = ['1.0.2', '1.2.0', '2.0.0'];
+  t.deepEqual(actual, expected);
+});
+
+test('find() multiple input versions', t => {
+  const input = ['1.0.0', '2.0.0'];
+  const packages = ['1.0.0', '1.0.1', '1.0.2', '1.1.0', '1.2.0', '2.0.0', '2.0.1'];
+  const actual = find(input, packages);
+  const expected = ['1.0.2', '1.2.0', '2.0.1'];
   t.deepEqual(actual, expected);
 });
